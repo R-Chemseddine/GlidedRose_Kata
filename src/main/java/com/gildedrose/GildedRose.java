dfd30238d0c8aa0b1e0e44be289229716af8f095
Kali <rachdiamine135@gmail.com>
2022-09-30 13:13:18 -0400

    Cleaned and fusionned ifs

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -21,27 +21,21 @@ class GildedRose {
                     item.quality++;
                 }
                 
-                if (item.sellIn < 0) {
-                    if (item.quality < 50) {
+                if (item.sellIn < 0 && item.quality < 50) {
                         item.quality++;
-                    }
                 }
                     break;
                 
                 case "Backstage passes to a TAFKAL80ETC concert":
-                if (item.quality < 50) {
-                    item.quality++;
-                }
-                    if (item.sellIn < 10) {
-                        if (item.quality < 50) {
-                            item.quality++;
-                        }
+                    if (item.quality < 50) {
+                        item.quality++;
+                    }
+                    if (item.sellIn < 10 && item.quality < 50) {
+                        item.quality++;
                     }
 
-                    if (item.sellIn < 5) {
-                        if (item.quality < 50) {
+                    if (item.sellIn < 5 && item.quality < 50) {
                             item.quality++;
-                        }
                     }
 
                     if (item.sellIn < 0) {
@@ -58,10 +52,8 @@ class GildedRose {
                         item.quality--;
                     }
 
-                    if (item.sellIn < -1) {
-                        if (item.quality > 0) {
-                            item.quality--;
-                        }
+                    if (item.sellIn < -1 && item.quality > 0) {
+                        item.quality--;
                     }
                     break;
             }
